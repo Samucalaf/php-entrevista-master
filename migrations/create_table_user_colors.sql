4d653d6738149d7088d611d34e26acd25e298a51
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-10-02 18:37:55 -0300

    feat: implementar relacionamento many-to-many entre users e colors

--- migrations/create_table_user_colors.sql
+++ migrations/create_table_user_colors.sql
@@ -1,4 +1,7 @@
 create table user_colors(
     user_id INTEGER NOT NULL,
-    color_id INTEGER NOT NULL
+    color_id INTEGER NOT NULL,
+    PRIMARY KEY (user_id, color,id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (color_id) REFERENCES colors(id) ON DELETE CASCADE
 )
