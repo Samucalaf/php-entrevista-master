8095d4cbd7b1e1b747b1969a06658eee36ae34fc
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-10-02 10:00:18 -0300

    teste criação de usuarios

--- seeds/insert_users_seed.sql
+++ seeds/insert_users_seed.sql
@@ -1,5 +1,5 @@
 INSERT INTO users(name, email)
     VALUES
-        ('Foo Bar', 'foo@bar'),
-        ('Bar Baz', 'bar@baz'),
-        ('Baz Foo', 'baz@foo')
+        ('Samuel', 'samuel@gmail.com'),
+        ('Barbara', 'barbara@gmail.com'),
+        ('Bruno', 'bruno@gmail.com')
